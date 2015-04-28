8cb0547cac798d7e39cae90c2a3ffaa6642a24b7
Phillip Azar <dain54@gmail.com>
2015-04-27 17:08:17 -0700

    Added /bluetooth package. Added BluetoothRecordToken.
     Changes to be committed:
    	new file:   bluetooth/BluetoothDevice.java
    	new file:   bluetooth/BluetoothRecordToken.java

---
+++ bluetooth/BluetoothRecordToken.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ptolemy.domains.wireless.lib.bluetooth;
+
+import ptolemy.data.RecordToken;
+
+///////////////////////////////////////////////////////////////////
+////BluetoothRecordToken
+
+/**
+A token that contains a set of label/token pairs pertinent to Bluetooth functions. Extension of this class
+is used to regulate input/output of the BluetoothDevice actor.
+TODO: Document!
+
+@author Phillip Azar
+@version 
+@since
+@Pt.ProposedRating 
+@Pt.AcceptedRating
+
+@see BluetoothDevice
+@see RecordToken
+*/
+public class BluetoothRecordToken extends RecordToken {
+    
+    /** Construct a BluetoothRecordToken with no fields.
+     */
+    public BluetoothRecordToken() {
+        super();
+        // TODO: Anything special here?
+    }
+
+}
